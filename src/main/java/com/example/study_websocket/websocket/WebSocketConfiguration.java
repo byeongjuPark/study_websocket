df9ac4cb9b7eda01476354af851c569241876ab5
byeongjuPark <bottlepark28@gmail.com>
2023-08-01 23:09:05 +0900

    웹 소켓 서버 구축

---
+++ src/main/java/com/example/study_websocket/websocket/WebSocketConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.study_websocket.websocket;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocket 
+public class WebSocketConfiguration implements WebSocketConfigurer {
+
+    private final WebSocketHandler webSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "/ws/chat")
+        .setAllowedOrigins("*");
+    }
+}
