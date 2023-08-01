0cc827c757c36055386d15b41993a61f7dd8165b
byeongjuPark <bottlepark28@gmail.com>
2023-08-01 22:48:27 +0900

    first commit

---
+++ src/main/java/com/example/study_websocket/StudyWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.study_websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyWebsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/study_websocket/StudyWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.study_websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
